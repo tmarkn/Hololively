e6fffef7ad7909ecaf1d70cc277a2ad345dcca4b
Tmarkn <39633888+tmarkn@users.noreply.github.com>
2020-10-03 21:15:03 -0400

    live button

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -10,6 +10,8 @@ let days = [
 ];
 
 let members = {};
+let live = [];
+let liveButton = $("#liveButton");
 // fetch data from api
 $.ajax({
     url: "/static/json/members.json",
@@ -32,47 +34,49 @@ let apiRequest = $.ajax({
 apiRequest.done(function (data) {
     // filter results
     let items = JSON.parse(data).streams;
-    let live = null;
-
     let calendarContainer = $("#calendarContainer");
-
+    let count = 0;
     // create objects
     items.forEach(stream => {
-        console.log(stream.time);
         let streamTime = parseTime(stream.time);
-        console.log(streamTime);
-        let dayContainer = $("#" + (streamTime.getMonth()+1) + "-" + streamTime.getDate());
+        // find day container
+        let dayContainer = $("#" + (streamTime.getMonth() + 1) + "-" + streamTime.getDate());
 
+        // create day container if not yet made
         if (!dayContainer.length) {
             dayContainer = $("<div/>", {
-                id: (streamTime.getMonth()+1) + "-" + streamTime.getDate(),
+                id: (streamTime.getMonth() + 1) + "-" + streamTime.getDate(),
                 class: "dayContainer",
             });
             dayContainer.appendTo(calendarContainer);
             let dayHeader = $("<h1/>", {
                 class: "dayHeader",
-                text: (streamTime.getMonth()+1) + "/" + streamTime.getDate() + '\n' + days[streamTime.getDay()],
+                text: (streamTime.getMonth() + 1) + "/" + streamTime.getDate() + '\n' + days[streamTime.getDay()],
             }).appendTo(dayContainer);
             dayHeader.html(dayHeader.html().replace(/\n/g, '<br/>'));
         }
 
+        // container
         let streamContainer = $("<div/>", {
-            id: stream.link,
+            id: stream.link.slice(32, stream.link.length),
             class: "streamContainer",
         }).appendTo(dayContainer);
 
+        // clickable link
         let clickable = $("<a/>", {
             class: "clickable",
             href: stream.link,
             target: '_blank',
         }).appendTo(streamContainer);
 
+        // thumbnail
         $("<img/>", {
             class: "thumbnail",
             src: stream.thumbnail,
             title: stream.host,
         }).appendTo(clickable);
 
+        // name
         let textContainer = $("<div/>", {
             class: "textContainer",
         }).appendTo(clickable);
@@ -80,36 +84,32 @@ apiRequest.done(function (data) {
         let memberName = $("<h2/>", {
             class: "memberName",
             text: stream.host,
-        }).appendTo(textContainer);
+        }).appendTo(textContainer)
+            .prepend($("<img/>", {
+                class: "avatar",
+                src: members[stream.collaborators[0]],
+                title: stream.host,
+            }));
 
-        memberName
-        .prepend($("<img/>", {
-            class: "avatar",
-            src: members[stream.collaborators[0]],
-            title: stream.host,
-        })
-        .appendTo(clickable));
-
+        // time
         $("<h2/>", {
             class: "streamTime",
             text: streamTime.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }),
         }).appendTo(textContainer);
 
+        // live
         if (stream.live) {
             streamContainer.addClass("live");
             memberName.append($("<span/>", {
                 class: "liveDot",
             }));
-            if (live === null) {
-                live = streamContainer;
-            }
+            live.push(streamContainer);
         }
     });
-    if (live) {
+    // scroll to live
+    if (live.length) {
         waitForElement(live, function () {
-            $('html, body').animate({
-                scrollTop: live.offset().top - 120
-            }, 1000, "swing");
+            liveButton.css("transform", "scale(1)");
         });
     }
 });
@@ -119,6 +119,24 @@ apiRequest.fail(function (a, b) {
     console.log(data);
 });
 
+// live button
+liveButton.on("mouseover", function () {
+    $(this).css("transform", "scale(1.35)");
+});
+
+liveButton.on("mouseleave", function () {
+    $(this).css("transform", "scale(1)");
+});
+
+liveButton.on("mousedown", function () {
+    $(this).css("transform", "scale(1.35)");
+    scrollToLive();
+    window.setTimeout(function () {
+        liveButton.css("transform", "scale(1)");
+    }, 100);
+});
+
+// swap dictionary key and values
 function swap(dict) {
     var reverse = {};
     for (var key in dict) {
@@ -127,18 +145,41 @@ function swap(dict) {
     return reverse;
 }
 
-function waitForElement(elementPath, callBack){
-    window.setTimeout(function(){
-      if($(elementPath).length){
-        callBack(elementPath, $(elementPath));
-      }else{
-        waitForElement(elementPath, callBack);
-      }
+// wait for element then do
+function waitForElement(elementPath, callBack) {
+    window.setTimeout(function () {
+        if ($(elementPath).length) {
+            callBack(elementPath, $(elementPath));
+        } else {
+            waitForElement(elementPath, callBack);
+        }
     }, 500);
 }
 
+// parse time and return date object
 function parseTime(timeString) {
     timeStr = timeString.slice(0, 10) + "T" + timeString.slice(11, timeString.length);
-    console.log(timeStr);
     return new Date(timeStr);
+}
+
+// scroll to live element
+let liveIndex = 0;
+function scrollToLive() {
+    if (live.length) {
+        let scrollPos = live[liveIndex].offset().top - 120;
+        let scrollTime = Math.abs($(document).scrollTop() - scrollPos) / 4;
+        if (scrollTime < 250) {
+            scrollTime = 250;
+        } else if (scrollTime > 550) {
+            scrollTime = 550;
+        }
+        $('html, body').stop();
+        $('html, body').animate({
+            scrollTop: scrollPos
+        }, scrollTime, "swing");
+    }
+    liveIndex += 1;
+    if (liveIndex >= live.length) {
+        liveIndex = 0;
+    }
 }
