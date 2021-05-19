af5838bee3721d9fe42a95f7410048de8dde9232
Tmarkn <tonymark1997@gmail.com>
2021-05-19 02:23:27 -0400

    google analytics

--- hololively.py
+++ hololively.py
@@ -1,8 +1,13 @@
-import json
 import markdown
+import os
 from api import API
 from datetime import datetime, timezone
+from dotenv import load_dotenv
 from flask import Flask, render_template, url_for, request
+
+load_dotenv()
+GA_MEASUREMENT_ID = os.getenv("GA_MEASUREMENT_ID")
+
 app = Flask(__name__, static_url_path='/static')
 
 with open('static/json/memberPhotos.json', 'r', encoding="utf-8") as f:
@@ -20,12 +25,12 @@ def home():
     query = request.args.get('q')
     if not query:
         query = ''
-    return render_template('calendar.html', title='Home', streams=API(query=query), memberPhotos=memberPhotos)
+    return render_template('calendar.html', title='Home', GA_MEASUREMENT_ID=GA_MEASUREMENT_ID, streams=API(query=query), memberPhotos=memberPhotos, )
 
 @app.route('/about/')
 @app.route('/policy/')
 def about():
-    return render_template('about.html', title='about', aboutText=markdown.markdown(aboutText), policyText=markdown.markdown(policyText))
+    return render_template('about.html', title='about', GA_MEASUREMENT_ID=GA_MEASUREMENT_ID, aboutText=markdown.markdown(aboutText), policyText=markdown.markdown(policyText))
 
 @app.route('/api/')
 @app.route('/api/<query>')
@@ -62,16 +67,16 @@ def api(query = ''):
 
 @app.route('/docs/')
 def docs():
-    return render_template('docs.html', title='Documentation', documentationText=markdown.markdown(documentationText))
+    return render_template('docs.html', title='Documentation', GA_MEASUREMENT_ID=GA_MEASUREMENT_ID, documentationText=markdown.markdown(documentationText))
 
 @app.route('/settings/')
 def settings():
-    return render_template('settings.html', title='Settings')
+    return render_template('settings.html', title='Settings', GA_MEASUREMENT_ID=GA_MEASUREMENT_ID)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html')
+    return render_template('404.html', GA_MEASUREMENT_ID=GA_MEASUREMENT_ID)
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host= '0.0.0.0')
